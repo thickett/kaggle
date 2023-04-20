49888ddf4cfddabb19d91dc0993d817434fbfbdb
thickett <82325813+thickett@users.noreply.github.com>
2023-04-20 20:15:56 +0100

    modified:   disaster tweets/__pycache__/app.cpython-38.pyc 	modified:   disaster tweets/app.py

--- disaster tweets/app.py
+++ disaster tweets/app.py
@@ -1,12 +1,18 @@
-from shiny import App,ui
+import matplotlib.pyplot as plt
+import numpy as np
+from shiny import App,ui, render
 
 
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n","choose a number n:",0,100,40)
+    ui.input_slider("n","choose a number n:",0,100,40),
+    ui.output_text_verbatim("txt"),
 )
 
 def server(input,output,session):
-    pass
+    @output
+    @render.text
+    def txt():
+        return f"The currrently selected value of n is: {input.n()}"
 
 app = App(app_ui,server)
