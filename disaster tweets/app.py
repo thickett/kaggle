c63287df7afed47a7ca3b9246cb6412279621a3b
thickett <82325813+thickett@users.noreply.github.com>
2023-04-22 12:35:05 +0100

    modified:   disaster tweets/__pycache__/app.cpython-38.pyc 	modified:   disaster tweets/app.py

--- disaster tweets/app.py
+++ disaster tweets/app.py
@@ -1,18 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from shiny import App,ui, render
-
+from shiny import ui, render, App, reactive
 
 
 app_ui = ui.page_fluid(
-    ui.input_slider("n","choose a number n:",0,100,40),
-    ui.output_text_verbatim("txt"),
+
+
 )
 
+
+
 def server(input,output,session):
-    @output
-    @render.text
-    def txt():
-        return f"The currrently selected value of n is: {input.n()}"
+    pass
 
 app = App(app_ui,server)
