e62234d9e856d6793ddaa5ae0566985ced520880
thickett <82325813+thickett@users.noreply.github.com>
2023-04-22 16:55:25 +0100

    modified:   disaster tweets/__pycache__/app.cpython-38.pyc 	modified:   disaster tweets/app.py

--- disaster tweets/app.py
+++ disaster tweets/app.py
@@ -32,15 +32,23 @@ app_ui = ui.page_fluid(
     #ui output element which we will call in the server to fill with our dataframe.
                         ui.output_ui("contents")
                 )
-    # next part of the UI is for column statistics and descriptions
-           ),ui.panel_title("Data Description"),
+    
+    
+    
+           ),
+           # next part of the UI is for column statistics and descriptions
+       
+           ui.panel_title("Data Description"),
            ui.layout_sidebar(
     
                 ui.panel_sidebar(
     # create a select object but with [] values. i.e we will dynamically update these depending on the cols.
-                    ui.input_select("dtype_select","Select a datatype to explore further.",[])
+                    ui.input_select("column_select","select a column to describe.",[])
                 ),
-                ui.panel_main()
+                    
+                ui.panel_main("hllooo",
+                    ui.output_ui("description")
+                )
            )
            
            
@@ -54,43 +62,58 @@ app_ui = ui.page_fluid(
 global_data = {}
 
 def server(input, output, session):
+
+    
     # server code to display the dataframe, and reactively update it if a new sampleis called.
     @output
     @render.ui
     def contents():
         if input.input_data() is None:
             return "Please upload a CSV"
+       
+        
         f: list[FileInfo] = input.input_data()
         df = pd.read_csv(f[0]["datapath"], header=0 if input.header() else None)
 
-        # Store the dataframe in the global dictionary
+            # Store the dataframe in the global dictionary
         global_data['df'] = df
 
-        # Call the dtype_select function explicitly
-        dtype_select()
+            # Call the dtype_select function explicitly
+        
+        column_select()
 
-        if input.Rerun_sample is None:
+        if input.Rerun_sample.get() ==0:
             df_head = df.head(10)
             return ui.HTML(df_head.to_html(classes=settings["table_layout"]))
 
         input.Rerun_sample()
         with reactive.isolate():
             return ui.HTML(df.sample(10).to_html(classes=settings["table_layout"]))
-
+        
+    @output
+    @render.ui   
+    def description():
+        df = global_data.get("df")
+        if df is not None:
+            desc = df.describe()
+            return ui.HTML(desc.to_html(classes=settings["table_layout"]))
     # function uses to dynmaically fill the dtype_select select button. 
     # notice how we call this function within the contents function and how we dont set any
     #handles on this. We do define it as an Effect function below though!
-    def dtype_select():
+    def column_select():
         # Access the dataframe from the global dictionary
         df = global_data.get('df')
         if df is not None:
             ui.update_select(
-            "dtype_select",  # Update the id parameter to match the input_select id
-            choices = [str(dtype) for dtype in set(df.dtypes)]
-        )
+            "column_select", 
+            choices = [str(dtype) for dtype in set(df.columns)]
 
+        )
     # Register the dtype_select function as a reactive effect
-    reactive.Effect(dtype_select)
+    reactive.Effect(column_select)
+  
+
+
 
 app = App(app_ui, server)
 
