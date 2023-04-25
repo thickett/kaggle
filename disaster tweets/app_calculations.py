e5b72d53a0a5c350989cd3eddeae33bfd670072d
thickett <82325813+thickett@users.noreply.github.com>
2023-04-25 20:07:51 +0100

    first draft of text pre-process logic added. 	modified:   disaster tweets/__pycache__/app_calculations.cpython-38.pyc 	modified:   disaster tweets/app_calculations.py 	modified:   disaster tweets/disaster_competition.ipynb

--- disaster tweets/app_calculations.py
+++ disaster tweets/app_calculations.py
@@ -3,6 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+import string
+from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize
+from nltk.corpus import stopwords
+import nltk
+import string
+
+nltk.download("punkt")
+nltk.download("stopwords")
+nltk.download("wordnet")
+nltk.download("omw-1.4")
 
 # set theme for all graphs to be that of seaborn.
 sns.set_theme()
@@ -42,7 +53,40 @@ class pre_process():
     
     def text_pre_process(self):
 
-        pass
+        '''takes a series of text bodies and tokenizes them, first into sentences, and then to words. 
+    It also takes some additional steps to remove stop words and punctuation.
+    Parameters
+    ----------
+    
+    text: An array-like of the shape (n_text_documents,)
+        An array that contains all the text to be tokenized.
+    return
+    ----------
+    
+    filtered_list: list
+        A list of lists that contains the tokenized words of each sentence in a body of text.
+        '''
+        # list to store outputted column(s)
+        temp_list = []
+        # list to store tokens for a specific column, these will then be appended to temp_list
+        # note: temp_list and filtered_list will be the same if on 1 column exists in self.text_cols.
+        filtered_list = []
+
+        english_stop_words = stopwords.words("english")
+        english_stop_words.extend(["said","would","could","also","get","go","like","try","part","also",
+                                "it's","that's","might","people","year","one","says"])
+        for col in self.text_cols:
+            text = self.df[col]
+            for i in text.index:
+
+                sent_tokens = sent_tokenize(text[i].lower())
+                removed_punctuation = [i.translate(str.maketrans('','',string.punctuation)) for i in sent_tokens]
+
+                word_tokens = [word_tokenize(i) for i in removed_punctuation]
+                filtered = [[word for word in sent if word not in english_stop_words and len(word) >3] for sent in word_tokens ]
+                filtered_list.append(filtered)
+            temp_list.append(filtered_list)
+        return  temp_list
 
     def categorical_pre_process(self):
         pass
@@ -52,4 +96,4 @@ class pre_process():
         # a copied version of the dataframe
         # then return this dataframe. That way we only copy the dataframe once, and then make one set
         # of changes, and return the new dataframe.
-        pass
+        text_columns = self.text_pre_process()
